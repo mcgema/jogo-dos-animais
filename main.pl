2f7da0c99dc080d75346907c01c518bd0dc3828c
Pedro Chamberlain <pedrochmatos@gmail.com>
2022-06-21 00:57:28 -0300

    add q&a methods to game

--- main.pl
+++ main.pl
@@ -1,6 +1,23 @@
 load :-
     consult('jogo_base.txt').
 
+procura_animal(Pergunta, Animal) :-
+    write(Pergunta), write(" (s./n.)"), nl,
+    read(InputJogador), nl,
+    (
+        (InputJogador=s, pergunta_s(Pergunta, NovaPergunta), procura_animal(NovaPergunta, Animal), !);
+        (InputJogador=s, not(pergunta_s(Pergunta, _)), animal_sim(Pergunta, Animal), !);
+        (InputJogador=n, pergunta_n(Pergunta, NovaPergunta), procura_animal(NovaPergunta, Animal), !);
+        (InputJogador=n, not(pergunta_n(Pergunta, _)), animal_nao(Pergunta, Animal), !)
+    ).
+
+verifica_resposta(Animal, InputJogador) :-
+    write("Por acaso seu animal é um... "), write(Animal), write("? (s./n.)"), nl, 
+    read(InputJogador), nl,
+    (
+        (InputJogador=s, write("Ganhei!"), nl);
+        (InputJogador=n, write("OK... Você venceu..."), nl)
+    ).
 
 save :-
     tell('jogo_atual.txt'),
@@ -13,6 +30,9 @@ play :-
     write("Eu vou tentar adivinhar qual animal você está pensando..."), nl, 
     
     load, 
+    pergunta_inicial(PerguntaInicial),
+    procura_animal(PerguntaInicial, Animal),
+    verifica_resposta(Animal, Resultado),
     save,
     write("Jogar novamente? (S/N): "), nl, 
     read(InputJogador),
