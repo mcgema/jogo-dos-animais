736d2d230e5965dc291a1506c18c8260a024598c
Pedro Chamberlain <pedrochmatos@gmail.com>
2022-06-21 00:55:03 -0300

    save values after game is over

--- main.pl
+++ main.pl
@@ -1,11 +1,19 @@
 load :-
     consult('jogo_base.txt').
 
+
+save :-
+    tell('jogo_atual.txt'),
+    listing(pergunta_inicial), listing(pergunta_s), listing(pergunta_n),
+    listing(animal_s), listing(animal_n),
+    told.
+
 play :-
     write("=== JOGO DOS ANIMAIS ==="), nl, write("Bem-vindo ao jogo dos animais!"), nl, nl,
     write("Eu vou tentar adivinhar qual animal você está pensando..."), nl, 
     
     load, 
+    save,
     write("Jogar novamente? (S/N): "), nl, 
     read(InputJogador),
     (   
